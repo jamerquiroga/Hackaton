603c7caa507603918f339e3772d5f2bdf75c06b9
Jhonatan Avalos <javalosg@everis.com>
2017-10-27 04:38:08 -0500

    AuthProvider : Inicio de sesion y usuario

---
+++ app/src/main/java/com/everis/hackaton/metropolitanoapp/model/base/User.java
@@ -0,0 +1,11 @@
+package com.everis.hackaton.metropolitanoapp.model.base;
+
+/**
+ * Created by jhonatanavalos on 10/27/17.
+ */
+
+public class User {
+    public long id = 0;
+    public String nombres = "";
+    public String apellidos = "";
+}
